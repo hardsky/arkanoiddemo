0da5b4d65f5853c016dedf1d781d747a27f93283
hardsky <hardsky@yandex.ru>
2013-10-27 11:40:08 +0400

    fix wall calculation

--- GameLayout.cpp
+++ GameLayout.cpp
@@ -3,17 +3,19 @@
 #include <string.h>
 
 namespace hsg {
-using namespace rapidxml;
+
+    using namespace rapidxml;
+
     GameLayout::GameLayout(CoordSystem* coordSystem){
 	width = coorSystem->right() - coorSystem->left();
 	height = coorSystem->top() - coorSystem->bottom();
 	center = coorSystem->center();
-	    
+			
 	wall.width = width * 0.8;
 	wall.heiht = height * 0.5;
-	    
+			
 	ball.diameter = width * 0.05;
-	    
+			
 	bat.width  = width * 0.25;
 	bat.height = height * 0.025;
 
@@ -23,7 +25,7 @@ using namespace rapidxml;
 
 	ball.center = bat.center;
 	ball.center.y += bat.height / 2 + ball.diameter / 2;
-	    
+			
 	wall.center.set(coorSystem->center().x,
 			coorSystem->top() - height * 0.1 -  wall.heiht / 2,
 			0);
@@ -40,7 +42,7 @@ using namespace rapidxml;
 	xml_document<> doc;
 	doc.parse<0>(sb);
 
-	
+
 	for(xml_node<> *node = doc.first_node("background"); node; node = node->next_sibling()){
 	    if(!strcmp(node->name(), "background")){
 		xml_attribute<> *attr = node->first_attribute();
@@ -78,24 +80,30 @@ using namespace rapidxml;
 	float brickWidth = wall.width / colCnt;
 	float brickHeight = wall.height / rowCnt;
 	
+	float centerX = wall.center.x;
 	float topEdge = wall.center.y + wall.height / 2;
-	float leftEdge = wall.center.x - wall.width / 2;
 	
 	float brickY = topEdge - brickHeight / 2;
 	for(int row  = 0; row < rowCnt; ++row){
-	    std::vector<std::string>& rowStr = wallStr[row];
-	    float brickX = leftEdge + wall.width / 2;
 
-	    for(int col = 0; col < colCnt; ++col){
-		BrickLayout brick;
-		brick.width = brickWidth - 1;
-		brick.height = brickHeight - 1;
-		brick.center.set(brickX, brickY, 0.0f);
-		brick.fileName = rowStr[col];
-		
-		wall.bricks.push_back(brick);		
+	    std::vector<std::string>& rowStr = wallStr[row];
+	    float rowWidth = rowStr.size() * brickWidth;
+	    float brickX = centerX - rowWidth / 2;
+
+	    for(int col = 0; col < rowStr.size(); ++col){
+
+		if(strcmp(rowStr[col], "hole")){ //not a hole
+		    BrickLayout brick;
+		    brick.width = brickWidth - 1;
+		    brick.height = brickHeight - 1;
+		    brick.center.set(brickX, brickY, 0.0f);
+		    brick.fileName = rowStr[col];
+
+		    wall.bricks.push_back(brick);
+		}		
 
 		brickX += brickWidth;
+
 	    }
 
 	    brickY += brickHeight;
